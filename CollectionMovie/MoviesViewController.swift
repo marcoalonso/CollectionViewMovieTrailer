147c6fc8ec779cd5ab2d9564794cc180a39c5a95
marcoalonso <marcoalonsorota@hotmail.com>
2022-06-01 12:22:23 -0500

    MasPeliculas

--- CollectionMovie/MoviesViewController.swift
+++ CollectionMovie/MoviesViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  CollectionMovie
-//
+// Animation: https://medium.com/@fabiogiolito/grid-animation-effects-in-swift-4d6ea4d6360d 
 //  Created by marco rodriguez on 01/06/22.
 //
 
@@ -10,33 +10,54 @@ import UIKit
 class MoviesViewController: UIViewController {
     
     var peliculaVer: Movie?
-    
     let animationDuration: Double = 0.3
     let delayBase: Double = 0.3
 
+    @IBOutlet weak var searchbarPeliculas: UISearchBar!
     @IBOutlet weak var collectionViewMovie: UICollectionView!
+    
+    var peliculasFiltradas: [Movie]!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        peliculasFiltradas = movies
+        
+        searchbarPeliculas.delegate = self
+        
         collectionViewMovie.delegate = self
         collectionViewMovie.dataSource = self
-        
-       
-        
     }
-
-
+}
+ // MARK: - SearchBar
+extension MoviesViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //inicializar lista filtrada en vacio
+        peliculasFiltradas = []
+        
+        if searchText == "" || searchText.isEmpty {
+            peliculasFiltradas = movies
+        } else {
+            for peli in movies {
+                if peli.titulo.lowercased().contains(searchText.lowercased()) {
+                    peliculasFiltradas.append(peli)
+                }
+            }
+        }
+        
+        self.collectionViewMovie.reloadData()
+    }
 }
 
+// MARK: - CollectionView
 extension MoviesViewController : UICollectionViewDelegate, UICollectionViewDataSource {
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let celda = collectionViewMovie.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as! MovieCollectionViewCell
 //        celda.alpha = 0
-        celda.configurar(peli: movies[indexPath.row])
+        celda.configurar(peli: peliculasFiltradas[indexPath.row])
         celda.movieImage.layer.cornerRadius = 10
         return celda
     }
@@ -55,13 +76,13 @@ extension MoviesViewController : UICollectionViewDelegate, UICollectionViewDataS
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return movies.count
+        return peliculasFiltradas.count
     }
 }
 
 extension MoviesViewController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 115, height: 170)
+        return CGSize(width: 130, height: 190)
     }
     
     
@@ -71,7 +92,7 @@ extension MoviesViewController: UICollectionViewDelegateFlowLayout{
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        peliculaVer = movies[indexPath.row]
+        peliculaVer = peliculasFiltradas[indexPath.row]
         performSegue(withIdentifier: "verDetalle", sender: self)
     }
     
